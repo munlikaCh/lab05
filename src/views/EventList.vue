498af40198e4108c3f9d7ed2afc61d857b1ed750
munlikaCh <munlika_ch@cmu.ac.th>
2021-08-11 15:45:14 +0700

    4.4

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -53,7 +53,7 @@ export default {
   // eslint-disable-next-line no-unused-vars
   beforeRouteEnter(routeTo, routeFrom,next){
     // NProgress.start()
-    EventService.getEvents(2,parseInt(routeTo.query.page) || 1)
+    EventService.getEvents(3,parseInt(routeTo.query.page) || 1)
       .then((response) => {
         next((comp)=>{
           comp.events = response.data
@@ -70,7 +70,7 @@ export default {
   beforeRouteUpdate(routeTo,routeFrom,next){ //ตอนแรกจารเขียนงี้
   // beforeRouteUpdate(routeTo){ //จารเขียน เปลี่ยนเมื่อไหร่ฟร่ะ
     // NProgress.start()
-    EventService.getEvents(2,parseInt(routeTo.query.page) || 1 )
+    EventService.getEvents(3,parseInt(routeTo.query.page) || 1 )
     .then((response)=>{
       this.events = response.data //<-----
       this.totalEvents = response.headers['x-total-count'] // <-----
@@ -99,7 +99,7 @@ export default {
   computed: {
     hasNextPage() {
       // First, calculate total pages
-      let totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per page
+      let totalPages = Math.ceil(this.totalEvents / 3) // 2 is events per page
 
       // Then check to see if the current page is less than the total pages.
       return this.page < totalPages
